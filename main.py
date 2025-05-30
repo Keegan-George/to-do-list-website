5fe36b84dc06ccc0e8dccfe192c89b57344f6795
Keegan-George <39040251+Keegan-George@users.noreply.github.com>
2025-05-30 11:13:02 -0400

    Add extensions.py file for extensions

--- main.py
+++ main.py
@@ -1,17 +1,9 @@
 from flask import Flask, render_template, redirect, url_for
 from to_do_form import ToDoForm
 from os import urandom
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
-from sqlalchemy import Integer, String, Date, ForeignKey
-from datetime import datetime
+from models import ToDoList, Task
+from extensions import db
 
-
-class Base(DeclarativeBase):
-    pass
-
-
-db = SQLAlchemy(model_class=Base)
 
 # create flask app
 app = Flask(__name__)
@@ -24,23 +16,6 @@ db.init_app(app)
 
 # secret key for csrf protection in flask form
 app.config["SECRET_KEY"] = urandom(32)
-
-
-class ToDoList(db.Model):
-    __tablename__ = "todo_list_table"
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    date: Mapped[datetime] = mapped_column(Date, unique=True, nullable=False)
-    tasks: Mapped[list["Task"]] = relationship(
-        back_populates="todo_list", cascade="all, delete-orphan"
-    )
-
-
-class Task(db.Model):
-    __tablename__ = "task_table"
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    title: Mapped[str] = mapped_column(String, nullable=False)
-    todo_list_id: Mapped[int] = mapped_column(ForeignKey("todo_list_table.id"))
-    todo_list: Mapped["ToDoList"] = relationship(back_populates="tasks")
 
 
 # create table schema in database
