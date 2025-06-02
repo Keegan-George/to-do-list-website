3a0a72c5d686b2866ec7fbe488b7eaa9d6e8f6ed
Keegan-George <39040251+Keegan-George@users.noreply.github.com>
2025-06-02 16:02:39 -0400

    Set complete flag to False be default when creating a task

--- helper.py
+++ helper.py
@@ -6,7 +6,7 @@ def add_task(date: date, task: str) -> None:
     """
     Add Task to a To-Do List. Creates a new To-Do List if one doesn't already exist.
     """
-    new_task = Task(title=task)
+    new_task = Task(title=task, complete=False)
 
     # check if a to-do list with that date exists
     todo_list: ToDoList = ToDoList.query.filter_by(date=date).first()
