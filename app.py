0f7b7ef2da4c527b19fac1b8b6ffb2aa7ab9b487
Keegan-George <39040251+Keegan-George@users.noreply.github.com>
2025-06-02 13:17:06 -0400

    Move database url to .env file. Add dedicated function for add a new task to a list

--- app.py
+++ app.py
@@ -6,12 +6,13 @@ from models import ToDoList, Task
 from flask import Flask, render_template, redirect, url_for
 from blueprints.delete import delete_bp
 
+
 def create_app():
     # create flask app
     app = Flask(__name__)
 
     # configure the SQLite database, relative to the app instance folder
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///todo.db"
+    app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL", "sqlite:///todo.db")
 
     # initialize the app with the extension
     db.init_app(app)
@@ -35,23 +36,33 @@ def create_app():
             date = form.date.data
             task = form.task.data
 
-            new_task = Task(title=task)
+            add_task(date, task)
 
-            existing_todo_list = ToDoList.query.filter_by(date=date).first()
-
-            if existing_todo_list:
-                existing_todo_list.tasks.append(new_task)
-                db.session.commit()
-
-            else:
-                new_todo_list = ToDoList(date=date)
-                new_todo_list.tasks.append(new_task)
-                db.session.add(new_todo_list)
-                db.session.commit()
             return redirect(url_for("home"))
 
         return render_template("index.html", form=form, todo_lists=todo_lists)
-    
+
+    def add_task(date, task):
+        """
+        Add Task to a To-Do List. Creates a new To-Do List if one doesn't already exist.
+        """
+        date = date
+        task = task
+
+        new_task = Task(title=task)
+
+        existing_todo_list: ToDoList = ToDoList.query.filter_by(date=date).first()
+
+        if existing_todo_list:
+            existing_todo_list.tasks.append(new_task)
+
+        else:
+            new_todo_list = ToDoList(date=date)
+            new_todo_list.tasks.append(new_task)
+            db.session.add(new_todo_list)
+
+        db.session.commit()
+
     app.register_blueprint(delete_bp, url_prefix="/delete")
 
     return app
