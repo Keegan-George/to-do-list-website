de773813db2509b4815ce610ca81c440dff681da
Keegan-George <39040251+Keegan-George@users.noreply.github.com>
2025-06-03 12:21:14 -0400

    Use first_or_404 to return a 404 error if the list is not found for any reason

--- helper.py
+++ helper.py
@@ -4,19 +4,18 @@ from models import db, Task, ToDoList
 
 def add_task(date: date, task: str) -> None:
     """
-    Add Task to a To-Do List. 
-    Creates a new To-Do List if one doesn't already exist for the given date.
+    Add task to an existing To-Do list.
+    Creates a new To-Do list if needed.
     """
-    new_task = Task(title=task, is_done=False)
+    # check if a to-do list with the provided date exists
+    todo_list: ToDoList = ToDoList.query.filter_by(date=date).first_or_404()
 
-    # check if a to-do list with that date exists
-    todo_list: ToDoList = ToDoList.query.filter_by(date=date).first()
-
-    # create to-do list if it does not exist
+    # create a new to-do list if needed
     if not todo_list:
         todo_list = ToDoList(date=date)
         db.session.add(todo_list)
 
+    new_task = Task(title=task, is_done=False)
     todo_list.tasks.append(new_task)
 
     db.session.commit()
