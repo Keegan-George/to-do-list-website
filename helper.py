a98aa9b26cb5958d96b05135ea9e91e0c8b399d0
Keegan-George <39040251+Keegan-George@users.noreply.github.com>
2025-06-03 11:33:03 -0400

    Modify doc string. Remove complete argument from add_task function

--- helper.py
+++ helper.py
@@ -2,11 +2,12 @@ from datetime import date
 from models import db, Task, ToDoList
 
 
-def add_task(date: date, task: str, complete: bool) -> None:
+def add_task(date: date, task: str) -> None:
     """
-    Add Task to a To-Do List. Creates a new To-Do List if one doesn't already exist.
+    Add Task to a To-Do List. 
+    Creates a new To-Do List if one doesn't already exist for the given date.
     """
-    new_task = Task(title=task, complete=complete)
+    new_task = Task(title=task)
 
     # check if a to-do list with that date exists
     todo_list: ToDoList = ToDoList.query.filter_by(date=date).first()
