63dd676fb330ac6156c1154f61d64b6cda2cd4b2
Keegan-George <39040251+Keegan-George@users.noreply.github.com>
2025-06-03 11:56:56 -0400

    Set is_done field to False when creating a new Task

--- helper.py
+++ helper.py
@@ -7,7 +7,7 @@ def add_task(date: date, task: str) -> None:
     Add Task to a To-Do List. 
     Creates a new To-Do List if one doesn't already exist for the given date.
     """
-    new_task = Task(title=task)
+    new_task = Task(title=task, is_done=False)
 
     # check if a to-do list with that date exists
     todo_list: ToDoList = ToDoList.query.filter_by(date=date).first()
